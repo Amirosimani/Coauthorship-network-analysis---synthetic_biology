a5e419d056df96f0b9c8a427bc4be5abd23016f5
amirosimani <amirhs.imani@gmail.com>
2016-08-01 12:42:24 -0400

    pre-community detection

--- second_attempt.R
+++ second_attempt.R
@@ -1,29 +1,27 @@
-### Libraries ----
+### 0. Libraries ----
 library(reshape)
 library(dplyr)
 library(stringr)
+library(stringdist)
 
-### Loading the file ----
-source_file = read.csv(file.choose(), sep = ";", header = F)
-
-### Functions ----
+### 1. Functions ----
 Matrix2Edge <- function(x){
-
-data <- x
-final <- data.frame("a"=character(),"b"=character())
-
-#the idea is to iterate over the columns of  the dataframe, melt it, and each time drop the melted column
-for (i in 1:(ncol(source_file)-1)){
-  id = paste("V", i, sep="")
-  edge_list <- melt(data, id = (id))
-  data[id] <- NULL
-  keeps <- c(id, "value")
-  edge_list <- edge_list[keeps]
-  colnames(edge_list) <- colnames(final)
-  final <- rbind(final,edge_list)
-}
-final <- final[!(final$b=="" | final$b==" "),]
-return(final)
+  
+  data <- x
+  final <- data.frame("a"=character(),"b"=character())
+  
+  #the idea is to iterate over the columns of  the dataframe, melt it, and each time drop the melted column
+  for (i in 1:(ncol(source_file)-1)){
+    id = paste("V", i, sep="")
+    edge_list <- melt(data, id = (id))
+    data[id] <- NULL
+    keeps <- c(id, "value")
+    edge_list <- edge_list[keeps]
+    colnames(edge_list) <- colnames(final)
+    final <- rbind(final,edge_list)
+  }
+  final <- final[!(final$b=="" | final$b==" "),]
+  return(final)
 }
 
 simmilarity_index <- function(x, y){
@@ -32,7 +30,10 @@ simmilarity_index <- function(x, y){
   return(sim_index)  
 }
 
-### Create the edge list ----
+### A.1. Loading the file ----
+source_file = read.csv(file.choose(), sep = ";", header = F)
+
+### A.2. Create the edge list ----
 edge_list <- Matrix2Edge(source_file)
 
 # triming and cleaning up cells
@@ -66,25 +67,30 @@ for (i in 1:nrow(edge_list)){
 
 write.csv(edge_list, file = "edge_list_synthbio.csv")
 
-### Network Analysis ----
+### B. Network Analysis ----
+#use the created edge list csv file "edge_list_synthbio.csv"
+edge_list = read.csv(file.choose(), sep = ",", header = T)
+edge_list$X <- NULL
+
 library(igraph)
 el=as.matrix(edge_list)
-g=graph.edgelist(el,directed=TRUE)
+graph=graph.edgelist(el,directed=TRUE)
 
 #calculating different degrees
-degrees <- data.frame(degree = degree(g),
-in_degree = degree(g, mode = c("in"), loops = FALSE, normalized = FALSE),
-out_degree = degree(g, mode = c("out"), loops = FALSE, normalized = FALSE),
-btwn= betweenness(g, directed = T),
-close = closeness(g, mode = c("all")),
-eigen <- evcent(g),
-bon <- bonpow(g)
+degrees <- data.frame(degree = degree(graph),
+in_degree = degree(graph, mode = c("in"), loops = FALSE, normalized = FALSE),
+out_degree = degree(graph, mode = c("out"), loops = FALSE, normalized = FALSE),
+btwn= betweenness(graph, directed = T),
+close = closeness(graph, mode = c("all")),
+eigen <- evcent(graph),
+bon <- bonpow(graph)
 )
 
 #cleaning up the table
 degrees = degrees[,c(1:6, 28)]
 
 # correlate the measures
+#turn it to a table
 cor(degrees)
 
 # regress attributes on centrality measures
@@ -93,14 +99,31 @@ summary(lm(in_degree ~ out_degree))
 ### Graph ----
 ## start the graph ##
 set.seed(12)
-l <- layout.kamada.kawai(g)
+l <- layout.kamada.kawai(graph)
 
-plot(lazega_friends_graph, layout=l, edge.arrow.size=.3)
+# Size node by in-degree.
+V(graph)$size <- 3*sqrt(degree(graph, mode="in"))
+V(graph)$size2 <- V(graph)$size * .2
+
+# Size node by in-betweenes
+V(graph)$size <- ((((betweenness(graph, directed = T)-23.832627)/4.566372)/5.2191602)+1)*2
+
+# Size of node label by in-degree.
+V(graph)$label.cex <- 2.5 * degree(graph, mode="in") / max(degree(graph, mode="in"))
+V(graph)$label.cex <- 0.001
+   # to do: make graph lables conditoined on in-betweeness
+
+
+# plot the graph
+plot(graph, layout=l, edge.arrow.size=.2, edge.curved=T, edge.color="grey")
+#title("Co-Authorship Graph",cex.main=0.8,col.main="black")
 
 
 ### to do----
   # transform entity resolution to a function
   # scraping journal articles to extract authors
+  # community detection
+
 
 
 
